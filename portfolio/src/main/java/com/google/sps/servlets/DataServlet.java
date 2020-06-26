e68782bd1a1de59930bd21b79788b2052b043ff8
LujieZ <lujie.sue.zhao@gmail.com>
2020-06-26 14:40:31 +0000

    Add comment section and profile picture

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -1,4 +1,4 @@
-// Copyright 2019 Google LLC
+ // Copyright 2019 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,19 +14,67 @@
 
 package com.google.sps.servlets;
 
+import com.google.sps.servlets.DataServlet;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.gson.Gson;
+import com.google.sps.data.Comment;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
-@WebServlet("/data")
+@WebServlet("/comment")
 public class DataServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello Sue!</h1>");
+    Query query = new Query("Comment").addSort("timestamp", SortDirection.DESCENDING);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+    
+    List<Comment> new_comments = new ArrayList<> ();
+    for (Entity entity : results.asIterable()) {
+        long new_id = entity.getKey().getId();
+        String new_content = (String) entity.getProperty("content");
+        long new_timestamp = (long) entity.getProperty("timestamp");
+
+        Comment new_comment = new Comment(new_id, new_content, new_timestamp);
+        new_comments.add(new_comment);
+    }
+
+    response.setContentType("application/json;"); // Send the JSON as the response
+    String json = new Gson().toJson(new_comments);
+    response.getWriter().println(json);
+
   }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // Post user comment if a comment is submitted
+        String comment = request.getParameter("user-comment");
+        long timestamp = System.currentTimeMillis();
+
+        // Create comment entity and store it in Datastore
+        Entity commentEntity = new Entity("Comment");
+        commentEntity.setProperty("content", comment);
+        commentEntity.setProperty("timestamp", timestamp);
+
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        datastore.put(commentEntity);
+
+        // Redirect back to the HTML page.
+        response.sendRedirect("/index.html");
+        
+    }
+  
 }
--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -13,16 +13,57 @@
 // limitations under the License.
 
 /**
- * Adds a random greeting to the page.
+ * Fetch the JSON string from the server and parse JSON
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+function parseSomething(){
 
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+    fetch('/comment')  // sends a request to /data
+    .then(response => response.json()) // parses the response as JSON
+    .then((object) => { // now we can reference the fields in myObject!
 
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+        const commentsListElement = document.getElementById('comments-container');
+        commentsListElement.innerHTML = '';
+
+        var numberCommentsDisplay = document.getElementById('number-comment').value;
+        numberCommentsDisplay = Math.min(numberCommentsDisplay, object.length);
+
+        for (var i = 0; i < numberCommentsDisplay; i++) {
+            commentsListElement.appendChild(createListElement(object[i]));
+        }
+        
+    });
+
+}
+
+/**
+    Delete a comment by its id 
+ */
+ function deleteComment(comment){
+     const params = new URLSearchParams();
+     params.append('id', comment.id);
+     fetch('/delete-data', {method: 'POST', body: params});
+ }
+
+/** Creates an <li> element containing text. */
+function createListElement(comment) {
+  const commentElement = document.createElement('li');
+  commentElement.className = 'comment';
+
+  const contentElement = document.createElement('span');
+  contentElement.innerText = comment.content;
+  
+  const deleteButtonElement = document.createElement('button');
+  deleteButtonElement.innerText = "goawayplease";
+  deleteButtonElement.addEventListener('click', () => {
+      deleteComment(comment);
+
+      // Remove the comment from the DOM.
+      commentElement.remove();
+      // parseSomething();
+  });
+
+  commentElement.appendChild(contentElement);
+  commentElement.appendChild(deleteButtonElement);
+
+  return commentElement;
 }
